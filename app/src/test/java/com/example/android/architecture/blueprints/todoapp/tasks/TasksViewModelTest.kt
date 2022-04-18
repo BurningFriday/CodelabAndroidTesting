5e956035d8eaf2a072528ad05cf2ae5b207fa69c
Hyunwoo Choi <corcow.choi@navercorp.com>
2022-04-19 03:35:50 +0900

    LiveDataTestAssertion (2) : LiveData assertion 추가
    (1) InstantTaskExecutorRule : JUnit 테스트 룰. 테스트에 사용되는 아키텍처 컴포넌트의 백그라운드 작업이 단일 스레드에서 동작하도록 함.
    
    (2) observeForever() : LiveData 구독 시 동작하므로, 강제로 임의의 observer를 구독하도록 함.
    테스트 종료 전 구독 해제 필수.

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,8 +1,14 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Assert.*
+import com.example.android.architecture.blueprints.todoapp.Event
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Rule
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,18 +17,33 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
 
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
     @Test
-    fun addNewTask() {
+    fun addNewTask_setsNewTaskEvent() {
 
         // Given a fresh ViewModel
-        // AndroidX Test 라이브러리로부터 applicationContext를 얻어와 뷰모델을 생성한다.
         val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
 
-        // When adding a new task
-        tasksViewModel.addNewTask()
+        // Create observer - no need for it to do anything!
+        val observer = Observer<Event<Unit>> {}
+        try {
 
-        // Then the new task event is triggered
-        // TODO test LiveData
+            // Observe the LiveData forever
+            tasksViewModel.newTaskEvent.observeForever(observer)
+
+            // When adding a new task
+            tasksViewModel.addNewTask()
+
+            // Then the new task event is triggered
+            val value = tasksViewModel.newTaskEvent.value
+            assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
+
+        } finally {
+            // Whatever happens, don't forget to remove the observer!
+            tasksViewModel.newTaskEvent.removeObserver(observer)
+        }
 
     }
 
